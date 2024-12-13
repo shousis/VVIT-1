0c9475486265fc6c252aa995f1a3d03ea543daad
shousis <egorgutnik17@gmail.com>
2024-12-13 13:30:29 +0300

    lab 7

---
+++ HW-LAB7/exercise1.py
@@ -0,0 +1,24 @@
+class UserAccount:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.__password = password  
+    
+    def set_password(self, new_password):
+        
+        self.__password = new_password
+    
+    def check_password(self, password):
+        
+        return self.__password == password
+
+
+user = UserAccount("deni", "deni@mail.ru", "anypassword")
+
+
+user.set_password= str(input("Enter your password:"))
+
+if(user.check_password):
+    print(user.check_password("newPassword"))  
+else:
+    print(user.check_password("incorrect_password"))       
